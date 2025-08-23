8281ff59f9bb5cc3334c98f5195e66786158ce68
planos <fernandostsilva286@gmail.com>
2025-08-23 10:10:23 -0300

    Delete src directory

--- src/lib/lumi.ts
+++
@@ -1,9 +0,0 @@
-
-import { createClient } from '@lumi.new/sdk'
-
-export const lumi = createClient({
-    "projectId": "p347791619280248832",
-    "apiBaseUrl": "https://api.lumi.new",
-    "authOrigin": "https://auth.lumi.new",
-})
-    
